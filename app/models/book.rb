ae4402e0725dc9577366e3664c1cb363ed47536b
jaceflournoy <jacehflournoy@gmail.com>
2019-04-23 17:16:57 -0500

    Fixed image upload

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,12 @@
 class Book < ApplicationRecord
   has_one_attached :photo
 
-  private def add_default_car_img
-    unless image.attached?
-      self.image.attach(io: File.open('app/assets/images/'), filename: 'default_picture.jpg', content_type: 'image/jpg')
+
+  before_create :add_default_book_img, on: [:create, :update]
+
+  private def add_default_book_img
+    unless photo.attached?
+      self.photo.attach(io: File.open('app/assets/images/default/default-picture.jpg'), filename: 'default-picture.jpg', content_type: 'image/jpg')
     end
   end
 
