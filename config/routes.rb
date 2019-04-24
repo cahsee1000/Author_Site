1fef2b757d2e2efc9bf43d040e0164b98e0936a6
Cassie Eggen <cleggen@uh.edu>
2019-04-23 21:04:45 -0500

    Fixed routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :users
+  resources :blogs
+  resources :events
+  resources :books
+  resources :contact_forms
+  resources :pages
+
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'book', to: 'books#index', as: 'book'
-  get 'event', to: 'events#index', as: 'event'
-  get 'blog', to: 'blogs#index', as: 'blog'
-  get 'home', to: 'pages#home', as: 'home'
 
   resources :sessions, only: [:new, :create, :destroy]
 
@@ -14,11 +16,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :blogs
-  resources :events
-  resources :books
-  resources :contact_forms
-  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  root 'pages#home'
 end
